b15fb529b1417fec7c02a8e0ab014f0f37e009d3
Ivan-yy <707300693@qq.com>
2019-12-17 14:17:17 +0800

    111

---
+++ src/test/java/com/yy/myblog/MyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yy.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
